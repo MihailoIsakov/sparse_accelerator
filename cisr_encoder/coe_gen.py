4729eb256ae701e30c9bef751faa9a7b94e83353
Mihailo Isakov <mihailo@protonmail.com>
2016-12-04 15:03:58 -0500

    extra comments when generating the coe file

--- cisr_encoder/coe_gen.py
+++ cisr_encoder/coe_gen.py
@@ -1,11 +1,15 @@
 
 
-def generate_coe(path, radix, memory, bytes_per_row=8):
+def generate_coe(path, memory, radix=16, bytes_per_row=8):
     """
     Creates a COE file
     """
     f = open(path, 'w')
 
+    f.write("; Storing NN weights and biases, along with images\n")
+    f.write("; Stored sequentially as W1, b1, W2, b2, W3, b3, img1, img2, img3...\n")
+    f.write("; To calculate the location of weights, biases or images, need to know their sizes!\n")
+    f.write("; Memory stored as one byte values, written in hex format (radix = 16)\n")
     f.write("memory_initialization_radix = " + str(radix) + "\n")
     f.write("memory_initialization_vector = \n")
 
