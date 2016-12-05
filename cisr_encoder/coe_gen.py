efa58d07e436987fd328231dda1aadae4290bff1
Mihailo Isakov <mihailo@protonmail.com>
2016-12-05 00:30:01 -0500

    Added addresses as comments, ; after the first parameter, assert.

--- cisr_encoder/coe_gen.py
+++ cisr_encoder/coe_gen.py
@@ -1,5 +1,5 @@
 
-def generate_coe(path, memory, radix=16, bytes_per_row=8):
+def generate_coe(path, memory, addr, radix=16, bytes_per_row=8):
     """
     Creates a COE file
     """
@@ -10,18 +10,19 @@ def generate_coe(path, memory, radix=16, bytes_per_row=8):
     f.write("; Positions of matrices and vectors are determined during synthesys,\n")
     f.write("; and should be parametrized in the design\n")
     f.write(";\n")
+    f.write("; Addresses of values, columns, lengths, biases, for the 3 layers: \n")
+    f.write("; " + str(addr) + "\n")
+    f.write(";\n")
     f.write("; Memory stored as one byte values, written in hex format (radix = 16)\n")
-    f.write("memory_initialization_radix = " + str(radix) + "\n")
+    f.write("memory_initialization_radix = " + str(radix) + ";\n")
     f.write("memory_initialization_vector = \n")
 
     row_counter = 0
     for cell in memory:
-        if not 0 <= cell <= 255:
-            print cell
-        # assert 0 <= cell <= 255
+        assert 0 <= cell <= 255
 
         # hex format
-        f.write(format(twos_complement(cell), '02x') + " ")
+        f.write(format(cell, '02x') + " ")
         # dec format
         # f.write(str(twos_complement(cell)) + " ")
 
