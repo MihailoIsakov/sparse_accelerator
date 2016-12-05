accccf15a9f43fc7860c8a7063d6a21953f92dfb
Mihailo Isakov <mihailo@protonmail.com>
2016-12-04 23:31:34 -0500

    Coe gen changes, saving hex and comments

--- cisr_encoder/coe_gen.py
+++ cisr_encoder/coe_gen.py
@@ -1,5 +1,4 @@
 
-
 def generate_coe(path, memory, radix=16, bytes_per_row=8):
     """
     Creates a COE file
@@ -8,15 +7,23 @@ def generate_coe(path, memory, radix=16, bytes_per_row=8):
 
     f.write("; Storing NN weights and biases, along with images\n")
     f.write("; Stored sequentially as W1, b1, W2, b2, W3, b3, img1, img2, img3...\n")
-    f.write("; To calculate the location of weights, biases or images, need to know their sizes!\n")
+    f.write("; Positions of matrices and vectors are determined during synthesys,\n")
+    f.write("; and should be parametrized in the design\n")
+    f.write(";\n")
     f.write("; Memory stored as one byte values, written in hex format (radix = 16)\n")
     f.write("memory_initialization_radix = " + str(radix) + "\n")
     f.write("memory_initialization_vector = \n")
 
     row_counter = 0
     for cell in memory:
-        assert cell < 256
-        f.write(format(cell, '02x') + " ")
+        if not 0 <= cell <= 255:
+            print cell
+        # assert 0 <= cell <= 255
+
+        # hex format
+        f.write(format(twos_complement(cell), '02x') + " ")
+        # dec format
+        # f.write(str(twos_complement(cell)) + " ")
 
         row_counter += 1
         if row_counter == bytes_per_row:
